baac7ae3a0468a35f90f9c825a9cb3cf6b0f59fb
actions@github.com <GitHub Actions>
2026-03-20 19:56:49 +0000

    zip ignores .git dir

--- tampermonkey-ajou-sso-theme.user.js
+++ tampermonkey-ajou-sso-theme.user.js
@@ -3,7 +3,7 @@
 // @description  Apply ajou sso login page theme.
 // @match        *://sso.ajou.ac.kr/*
 // @author       Hyeonseung Kang <@hyeonseungkang>
-// @version      1774036381191
+// @version      1774036609253
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ajou.ac.kr
 // @grant        none
 // @inject-into  content
